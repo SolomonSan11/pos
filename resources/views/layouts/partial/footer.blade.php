38a9f5b8ba969cb1e92bcfd530596e68379b2a33
SolomonSan11 <darthvedar11@gmail.com>
2022-11-21 23:13:28 +0630

    [insert]backend dashboard

---
+++ resources/views/layouts/partial/footer.blade.php
@@ -0,0 +1,21 @@
+   <!-- BEGIN GLOBAL MANDATORY SCRIPTS -->
+   <script src="/backend/assets/js/libs/jquery-3.1.1.min.js"></script>
+   <script src="/backend/bootstrap/js/popper.min.js"></script>
+   <script src="/backend/bootstrap/js/bootstrap.min.js"></script>
+   <script src="/backend/plugins/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+   <script src="/backend/assets/js/app.js"></script>
+   <script>
+       $(document).ready(function() {
+           App.init();
+       });
+   </script>
+   <script src="assets/js/custom.js"></script>
+   <!-- END GLOBAL MANDATORY SCRIPTS -->
+
+   <!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM SCRIPTS -->
+   <script src="/backend/plugins/apex/apexcharts.min.js"></script>
+   <script src="/backend/assets/js/dashboard/dash_1.js"></script>
+   <!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM SCRIPTS -->
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Backend\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,32 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['prefix' => 'admin','middleware' => ['auth'],'name'=> 'admin'], function (){
+
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+
 });
